b4d747719288142cffbdf87274479d802c6f2c5a
vincentcarbonaro <nd4spd356@gmail.com>
2015-07-21 00:30:44 -0400

    Insertion-Sort.js

--- 3-Javascript-Solutions/12-insertion-sort-sol.js
+++ 3-Javascript-Solutions/12-insertion-sort-sol.js
@@ -1,6 +1,13 @@
 // Implement Insertion Sort
 
 function insSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+        var k = arr[i];
+        for (var j = i; j > 0 && k < arr[j - 1]; j--)
+            arr[j] = arr[j - 1];
+            arr[j] = k;
+    }
+    return arr;
 }
 
 console.log();
